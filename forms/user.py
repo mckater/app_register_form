220ce43222c8c7e256f49b60beb567fee57045b4
localuser <myfreer@mail.ru>
2024-03-02 11:08:26 +0300

    generate_password_hash, check_password_hash

--- forms/user.py
+++ forms/user.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField
 from wtforms.validators import DataRequired
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class RegisterForm(FlaskForm):
@@ -10,3 +11,9 @@ class RegisterForm(FlaskForm):
     name = StringField('Имя пользователя', validators=[DataRequired()])
     about = TextAreaField("Немного о себе")
     submit = SubmitField('Войти')
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
